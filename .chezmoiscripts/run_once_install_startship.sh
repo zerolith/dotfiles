3abc7e9830c12a7ff505f8965a570896942d4fce
Daniel Ackermann <d.ackermann@sonnen.de>
2024-02-03 00:31:54 +0100

    Testing for Docker deploy

---
+++ .chezmoiscripts/run_once_install_startship.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if  command -v starship >/dev/null 2>&1; then 
+    echo "starship already installed"
+else
+    curl -fsSL https://starship.rs/install.sh | sudo sh
+fi
+chsh -s $(which zsh)
